e3c28cd7e6d505fc521d6e3448f872a00e624587
tracer-ics2022 <tracer@njuics.org>
2022-10-02 12:25:57 +0800

    >  compile NEMU 211300091 贾劭鹏 Linux jiakechong-Lenovo-Legion-R70002021 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:25:57 up  3:01,  1 user,  load average: 0.96, 1.00, 1.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -278,7 +278,7 @@ word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
     return 0;}
-return eval(0,sizeof(tokens));}
+return eval(0,2);}
 
   /* TODO: Insert codes to evaluate the expression. */
   
